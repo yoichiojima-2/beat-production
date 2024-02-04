4ffa01dee529c3495c30be897a31d3c4bb5615ac
Yoichi Ojima <yoichiojima@gmail.com>
2024-02-04 23:15:49 +0900

    simplify mode module
    - refactor / implement with class

--- notebooks/modes.py
+++ notebooks/modes.py
@@ -1,78 +1,32 @@
-def lydian(key: int) -> list[int]:
-    return [
-        key,
-        key + 2,
-        key + 4,
-        key + 6,
-        key + 7,
-        key + 9,
-        key + 11
-    ]
+from dataclasses import dataclass
 
-def ionian(key: int) -> list[int]:
-    return [
-        key,
-        key + 2,
-        key + 4,
-        key + 5,
-        key + 7,
-        key + 9,
-        key + 11
-    ]
+@dataclass
+class Mode:
+    key: int
 
-def mixolydian(key: int) -> list[int]:
-    return [
-        key,
-        key + 2,
-        key + 4,
-        key + 5,
-        key + 7,
-        key + 9,
-        key + 10
-    ]
+    def build_notes(self, intervals: list[int]):
+        notes: list[int] = []
+        for i in intervals:
+            notes.append(self.key + i)
+        return notes
 
-def dorian(key: int) -> list[int]:
-    return [
-        key,
-        key + 2,
-        key + 3,
-        key + 5,
-        key + 7,
-        key + 9,
-        key + 10
-    ]
+    def lydian(self) -> list[int]:
+        return self.build_notes([0, 2, 4, 6, 7, 9, 11])
 
+    def ionian(self) -> list[int]:
+        return self.build_notes([0, 2, 4, 5, 7, 9, 11])
+    
+    def mixolydian(self) -> list[int]:
+        return self.build_notes([0, 2, 4, 5, 7, 9, 10])
+    
+    def dorian(self) -> list[int]:
+        return self.build_notes([0, 2, 3, 5, 7, 9, 10])
+    
+    def aeolian(self) -> list[int]:
+        return self.build_notes([0, 2, 3, 5, 7, 8, 10])    
+    
+    def phrygian(self) -> list[int]:
+        return self.build_notes([0, 1, 3, 5, 7, 8, 10])   
 
-def aeolian(key: int) -> list[int]:
-    return [
-        key,
-        key + 2,
-        key + 3,
-        key + 5,
-        key + 7,
-        key + 8,
-        key + 10
-    ]
-
-
-def phrygian(key: int) -> list[int]:
-    return [
-        key,
-        key + 1,
-        key + 3,
-        key + 5,
-        key + 7,
-        key + 8,
-        key + 10
-    ]
-
-def locrian(key: int) -> list[int]:
-    return [
-        key,
-        key + 1,
-        key + 3,
-        key + 5,
-        key + 6,
-        key + 8,
-        key + 10
-    ]
+    def locrian(self) -> list[int]:
+        return self.build_notes([0, 1, 3, 5, 6, 8, 10])
